c7ec4d8f73c481a6171acd865c05908dbc72e015
Shreyash <shreyashyaduvanshi@gmail.com>
2023-06-05 01:08:25 +0530

    update 6 files, create 282 files and copy 1 file

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,16 +1,30 @@
 // Node server for handling socket io connections
 
-const io = require('socket.io')(3000);
-
+const io = require('socket.io')(8000, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
 const users = {};
 
 io.on('connection', socket => {
     socket.on('new-user-joined', name => {
+        // console.log("New User ", name);
         users[socket.id] = name;
         socket.broadcast.emit('user-joined', name);
     });
 
     socket.on('send', message => {
-        socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
+        socket.broadcast.emit('receive', {
+            message: message,
+            name: users[socket.id] 
+        });
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left',  users[socket.id]);
+        delete users[socket.id];
     });
 });
+
